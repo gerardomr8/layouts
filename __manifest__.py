b7048223794fe1383716c5087b6e94c925224105
mathorif <astaroth7sigma@gmail.com>
2021-06-28 13:25:08 -0500

    Subiendo

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'layouts capacitacion',
+    'summary': 'layout set for website',
+    'version': '14.0.1',
+    'category': 'Website',
+    'author': 'Quadit, ',
+    'website': 'https://www.quadit.mx',
+    'license': 'LGPL-3',
+    'depends': ['base','website'],
+    'data': [
+            'views/header_banner.xml',            
+            'views/templates.xml',
+            'views/home.xml',
+            'views/fields_login.xml',
+            'views/btn_signup.xml',
+            'views/header_linea.xml',
+            'views/signup.xml',
+            'views/footer.xml',
+            ],
+    'demo': [],
+    'development_status': 'alpha',
+    'maintainers': [
+        'gerardomr8'
+    ],
+    'installable': True,
+}
